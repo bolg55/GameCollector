f6e812eb59f755c3006d1f3fac3c65487193f0cd
Kellen Bolger <bolger.kellen@gmail.com>
2018-09-09 18:31:56 -0400

    Initial Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Kellen Bolger on 2018-09-09.
+//  Copyright © 2018 Kellen Bolger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
